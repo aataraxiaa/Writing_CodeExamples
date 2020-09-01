a4e9b43c12940664d9ed7c338d61a12a84d64703
Pete Smith <peadar81@gmail.com>
2020-09-01 16:53:28 +0100

    Iterate on sample code

--- AppOnboardingWithSwiftUI/Shared/ContentView.swift
+++ AppOnboardingWithSwiftUI/Shared/ContentView.swift
@@ -9,11 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @AppStorage("needsOnboarding") var needsOnboarding: Bool = true
+    @AppStorage("needsAppOnboarding") var needsAppOnboarding: Bool = true
     
     var body: some View {
         mainView.onAppear {
-            if !needsOnboarding {
+            
+            /*
+             Flow #1:
+             
+             When a user has already onboarded to the app, i.e not the first launch, we perform
+             any necessary app setup when our main view appears.
+             
+             We will know the user has already onboarded as the needsAppOnboarding flag will be false.
+            */
+            
+            if !needsAppOnboarding {
                 // Set up app here!
             }
         }
@@ -24,10 +34,25 @@ extension ContentView {
     
     private var mainView: some View {
         Text("Hello, world!")
-            .padding()
-            .sheet(isPresented:$needsOnboarding) {
-                OnboardingView()
+        .padding()
+        .sheet(isPresented:$needsAppOnboarding) {
+            OnboardingView()
+        }
+        .onChange(of: needsAppOnboarding) { needsAppOnboarding in
+            
+            /*
+             Flow #2:
+             
+             When the user has just onboarded as part of this app launch, the needsAppOnboarding flag will change.
+             Using the .onChange view modifier, we can react to this change, and perform any necessary app setup.
+             
+             Again, we will know the user has already onboarded as the needsAppOnboarding flag will now be false.
+            */
+            
+            if !needsAppOnboarding {
+                // Set up app here!
             }
+        }
     }
 }
 
--- AppOnboardingWithSwiftUI/Shared/Onboarding/OnboardingView.swift
+++ AppOnboardingWithSwiftUI/Shared/Onboarding/OnboardingView.swift
@@ -7,33 +7,61 @@
 
 import SwiftUI
 
+// This is our main Onboarding view
+
 struct OnboardingView: View {
     
     var body: some View {
+        
+        // The view consists of a vertical stack of elements
         VStack {
             Spacer(minLength: 150)
-            Image(systemName: "circle")
+            Image(systemName: "wand.and.stars")
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 80, height: 80, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-            Text("Welcome To Bikey")
+            Text("Welcome To My App")
                 .font(Font.title2.bold().lowercaseSmallCaps())
                 .multilineTextAlignment(.center)
             Spacer(minLength: 60)
-            Text("Supports Over 400 Cities Worldwide")
+            Text("Something something this app 🤪")
             Spacer(minLength: 30)
-            Text("Safe & Secure")
+            Text("And another something!")
             Spacer(minLength: 30)
-            Text("Built With Love ❤️")
+            Text("And finally 🥳...some...thing")
+            
+            // We use a custom button view to allow the user to complete onboarding
+            OnboardingButton()
         }
-        .background(Color.blue)
-        .foregroundColor(.red)
+        .background(Color.gray)
+        .foregroundColor(.white)
         .ignoresSafeArea(.all, edges: .all)
     }
 }
 
-struct OnboardingView_Previews: PreviewProvider {
-    static var previews: some View {
-        OnboardingView()
+
+// This is our custom Onboarding button view, used to allow the user to complete onboarding
+struct OnboardingButton: View {
+    
+    @AppStorage("needsAppOnboarding") var needsAppOnboarding: Bool = true
+    
+    var body: some View {
+        GeometryReader { proxy in
+            LazyHStack {
+                Button(action: {
+                    needsAppOnboarding = false
+                }) {
+                    Text("Finish Setup")
+                    .padding(.horizontal, 40)
+                    .padding(.vertical, 15)
+                    .font(Font.title2.bold().lowercaseSmallCaps())
+                }
+                .background(Color.white)
+                .foregroundColor(.black)
+                .cornerRadius(40)
+                .frame(minWidth: 0, maxWidth: proxy.size.width-40)
+            }
+            .frame(width: proxy.size.width, height: proxy.size.height/1.5)
+        }
     }
 }
