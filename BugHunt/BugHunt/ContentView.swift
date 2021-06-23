3b172f1777600a3d926286b5c77b772d188ec7b6
Pete Smith <peadar81@gmail.com>
2021-06-23 17:20:22 +0100

    Update UI

--- BugHunt/BugHunt/ContentView.swift
+++ BugHunt/BugHunt/ContentView.swift
@@ -12,21 +12,28 @@ struct ContentView: View {
     @State var isPresented = false
 
     var body: some View {
-        Text("Welcome to Bug Hunt!")
-            .padding()
+        VStack {
+            Text("Welcome to Bug Hunt!")
+                .padding()
 
-        Button(action: {
-            isPresented = true
-        }, label: {
-            Text("Show me a bug")
-        })
-        .alert(isPresented: $isPresented, content: {
-            Alert(title: Text("Show Bug!"),
-                  message: Text("This is a bug 🐛"),
-                  dismissButton: .default(Text("Ok"))
-            )
-        })
-
+            Button(action: {
+                isPresented = true
+            }, label: {
+                Text("Show me a bug")
+            })
+                .padding()
+                .background(Color.black)
+                .cornerRadius(20)
+                .alert(isPresented: $isPresented, content: {
+                    Alert(title: Text("Show Bug!"),
+                          message: Text("This is a bug 🐛"),
+                          dismissButton: .default(Text("Ok"))
+                    )
+                })
+        }
+        .edgesIgnoringSafeArea(.all)
+        .frame(minWidth: 0, idealWidth: .greatestFiniteMagnitude, maxWidth: .greatestFiniteMagnitude, minHeight: 0, idealHeight: .greatestFiniteMagnitude, maxHeight: .greatestFiniteMagnitude, alignment: .center)
+        .background(Color.cyan)
     }
 }
 
